de11e135ee1f0fea12a33bbacdc9726a3798cd42
yonasadiel <yonazadielwiguna@gmail.com>
2017-11-06 19:31:24 +0700

    add animate and allow setEdge

--- src/inc/poly2.h
+++ src/inc/poly2.h
@@ -7,6 +7,7 @@
 #define POLY2_H
 
 #include "titik2.h"
+#define ANIMATION_STEP 60
 
 class Poly2
 {
@@ -25,17 +26,23 @@ public:
   Titik2 getCorners();
   /* get i-th corner */
   Titik2 getCorner(int i);
+  /* change poly to have n edge */
+  void setEdge(int n);
   /* move i-th corner to corner */
   void setCorner(int i, Titik2 corner);
 
   /* translate the Poly2 */
   void translate(float dx, float dy);
+  void animateTranslate(float dx, float dy, int step);
   /* dilate the Poly2 from (0,0) */
   void dilate(float k);
+  void animateDilate(float k, int step);
   /* rotate the Poly2 from (0,0) */
   void rotateOrigin(float deg);
+  void animateRotateOrigin(float deg, int step);
   /* rotate the Poly2 from (c_x,c_y) */
   void rotate(float deg, float c_x, float c_y);
+  void animateRotate(float deg, float c_x, float c_y, int step);
   /* reflect the Poly2 by line type:
    * 0 is absis
    * 1 is ordinate
@@ -47,17 +54,22 @@ public:
   void reflectByPoint(float c_x, float c_y);
   /* shear the Poly2 in x direction by k factor */
   void shearByX(float k);
+  void animateShearByX(float k, int step);
   /* shear the Poly2 in y direction by k factor */
   void shearByY(float k);
+  void animateShearByY(float k, int step);
   /* shear the Poly2 in x direction by k factor */
   void stretchByX(float k);
+  void animateStretchByX(float k, int step);
   /* shear the Poly2 in y direction by k factor */
   void stretchByY(float k);
+  void animateStretchByY(float k, int step);
   /* transform the Poly2 by Matriks2 m
    * m element is
    * a b 
    * c d */
   void customTransform(float a, float b, float c, float d);
+  void animateCustomTransform(float a, float b, float c, float d, int step);
   /* transform the Titik2 by Matriks2 m
    * m is valid Matriks2 */
   void transform(Matriks2 m);
