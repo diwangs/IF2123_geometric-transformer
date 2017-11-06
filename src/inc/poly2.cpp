9b00f05f69c07c2b2313b17ed341354660dc7fb4
yonasadiel <yonazadielwiguna@gmail.com>
2017-11-06 19:31:24 +0700

    add animate and allow setEdge

--- src/inc/poly2.cpp
+++ src/inc/poly2.cpp
@@ -21,6 +21,13 @@ Titik2 Poly2::getCorner(int i) {
   return m_corners[i];
 }
 
+void Poly2::setEdge(int n) {
+  Titik2* m_corners_old = m_corners;
+  Titik2* m_corners_new = (Titik2*) realloc(m_corners, n * sizeof(Titik2));
+  m_corners = m_corners_new;
+  free(m_corners_old);
+}
+
 void Poly2::setCorner(int i, Titik2 corner) {
   m_corners[i] = corner;
 }
@@ -33,12 +40,20 @@ void Poly2::translate(float dx, float dy) {
   }
 }
 
+void Poly2::animateTranslate(float dx, float dy, int step) {
+  translate(dx*step/60, dy*step/ANIMATION_STEP);
+}
+
 void Poly2::dilate(float k) {
   for (int i=0; i<getEdge(); i++) {
     Titik2 corner = getCorner(i);
     corner.dilate(k);
     setCorner(i, corner);
   }
+}
+
+void Poly2::animateDilate(float k, int step) {
+  dilate(k*step/ANIMATION_STEP);
 }
 
 void Poly2::rotateOrigin(float deg) {
@@ -49,12 +64,20 @@ void Poly2::rotateOrigin(float deg) {
   }
 }
 
+void Poly2::animateRotateOrigin(float deg, int step) {
+  rotateOrigin(deg*step/ANIMATION_STEP);
+}
+
 void Poly2::rotate(float deg, float c_x, float c_y) {
   for (int i=0; i<getEdge(); i++) {
     Titik2 corner = getCorner(i);
     corner.rotate(deg, c_x, c_y);
     setCorner(i, corner);
   }
+}
+
+void Poly2::animateRotate(float deg, float c_x, float c_y, int step) {
+  rotate(deg*step/ANIMATION_STEP, c_x, c_y);
 }
 
 void Poly2::reflectByLine(int type) {
@@ -81,12 +104,20 @@ void Poly2::shearByX(float k) {
   }
 }
 
+void Poly2::animateShearByX(float k, int step) {
+  shearByX(k*step/ANIMATION_STEP);
+}
+
 void Poly2::shearByY(float k) {
   for (int i=0; i<getEdge(); i++) {
     Titik2 corner = getCorner(i);
     corner.shearByY(k);
     setCorner(i, corner);
   }
+}
+
+void Poly2::animateShearByY(float k, int step) {
+  shearByX(k*step/ANIMATION_STEP);
 }
 
 void Poly2::stretchByX(float k) {
@@ -97,6 +128,10 @@ void Poly2::stretchByX(float k) {
   }
 }
 
+void Poly2::animateStretchByX(float k, int step) {
+  stretchByX(k*step/ANIMATION_STEP);
+}
+
 void Poly2::stretchByY(float k) {
   for (int i=0; i<getEdge(); i++) {
     Titik2 corner = getCorner(i);
@@ -105,12 +140,25 @@ void Poly2::stretchByY(float k) {
   }
 }
 
+void Poly2::animateStretchByY(float k, int step) {
+  stretchByY(k*step/ANIMATION_STEP);
+}
+
 void Poly2::customTransform(float a, float b, float c, float d) {
   for (int i=0; i<getEdge(); i++) {
     Titik2 corner = getCorner(i);
     corner.customTransform(a, b, c, d);
     setCorner(i, corner);
   }
+}
+
+void Poly2::animateCustomTransform(float a, float b, float c, float d, int step) {
+  customTransform(
+    a*step/ANIMATION_STEP,
+    b*step/ANIMATION_STEP,
+    c*step/ANIMATION_STEP,
+    d*step/ANIMATION_STEP
+  );
 }
 
 void Poly2::transform(Matriks2 m) {
